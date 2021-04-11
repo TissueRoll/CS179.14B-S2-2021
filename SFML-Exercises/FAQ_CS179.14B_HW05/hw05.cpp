f278fec3ad952f24d971ef9ab3a4afc8777f0c11
TissueRoll <christopher.dizon@protonmail.com>
2021-04-11 18:21:12 +0800

    added comments for hw03

--- SFML-Exercises/FAQ_CS179.14B_HW05/hw05.cpp
+++ SFML-Exercises/FAQ_CS179.14B_HW05/hw05.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <cmath> // pow
 #include <fstream>
 #include <vector>
 #include <SFML/Graphics.hpp>
@@ -28,11 +29,14 @@ constexpr float rad_to_deg{180.f/pi};
 namespace default_vals {
     constexpr unsigned int window_w{1500};
     constexpr unsigned int window_h{900};
+    constexpr unsigned int curve_order{2};
     constexpr float c_radius{10.f};
     constexpr float smoothness{10.f};
     constexpr unsigned int control_points{3};
     constexpr int curves = (control_points-1)/2;
     constexpr int points = curves * smoothness + 1;
+    constexpr int tanNorm{3};
+    constexpr int tan_points = curves * tanNorm + 1;
 }
 
 template <typename T>
@@ -45,18 +49,11 @@ T cross (const sf::Vector2<T>& a, const sf::Vector2<T>& b) {
     return a.x*b.y - b.x*a.y;
 }
 
-// __NOTES__
-// linear interpolation function
-// takes in 2 vectors (start and end), and some value t with domain [0,1]
-// see slide 3 for clarification
 template <typename T>
 sf::Vector2<T> lerp(const sf::Vector2<T>& v0, const sf::Vector2<T>& v1, T t) {
 	return v0 + ((v1 - v0) * t);
 }
 
-// __NOTES__
-// bezier curve interpolation
-// see slide 12 for clarification
 template <typename T>
 sf::Vector2<T> make_curve(const sf::Vector2<T>& v0, const sf::Vector2<T>& v1, const sf::Vector2<T>& v2, T t) {
 	return lerp(lerp(v0, v1, t), lerp(v1, v2, t), t);
@@ -68,12 +65,16 @@ enum Direction {up, down, left, right};
 // globals
 unsigned int window_w{default_vals::window_w};
 unsigned int window_h{default_vals::window_h};
+int curve_order{default_vals::curve_order};
 float c_radius{default_vals::c_radius};
 float smoothness{default_vals::smoothness};
-unsigned int control_points{default_vals::control_points};
+int control_points{default_vals::control_points};
 int curves{default_vals::curves};
 int points{default_vals::points};
 float inv_smoothness{1.f/smoothness};
+int tanNorm{default_vals::tanNorm};
+int tan_points{default_vals::tan_points};
+float inv_tanNorm{1.f/tanNorm};
 
 bool directionFlags[4] = {false, false, false, false};
 bool leftMouseButtonFlag = false;
@@ -81,26 +82,112 @@ bool leftMouseButtonFlag = false;
 std::vector<sf::CircleShape> circles;
 sf::VertexArray ctrlPoints{sf::LineStrip};
 sf::VertexArray allPoints{sf::LineStrip};
+sf::VertexArray tanPoints{sf::Lines};
+sf::VertexArray normalPoints{sf::Lines};
 std::vector<bool> circlesFlags;
+std::vector<std::vector<long long>> pascal;
+std::vector<std::vector<float>> poly_coefs;
+std::vector<std::vector<float>> tsrc_coefs;
+std::vector<std::vector<float>> tang_coefs;
 
-// __NOTES__
-// updates the bezier curve based on some index
-// this is a lazy update (lazy means you only update on demand)
+void updatePascalTriangle(unsigned int order) {
+    pascal.resize(order+1);
+    for (unsigned int i = 0; i <= order; ++i) {
+        pascal[i].resize(i+1);
+        pascal[i][0] = pascal[i][i] = 1;
+        for (unsigned int j = 1; j < i; ++j) {
+            pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+        }
+    }
+}
+
+void updatePolyCoefs(unsigned int level, unsigned int order) {
+    updatePascalTriangle(order);
+    poly_coefs.resize(level+1);
+    for (unsigned int i = 0; i <= level; ++i) {
+        poly_coefs[i].resize(order+1);
+        float g = inv_smoothness * i;
+        float f = 1.f - g;
+        for (unsigned int j = 0; j <= order; ++j) {
+            poly_coefs[i][j] = pascal[order][j] * std::pow(f, order - j) * std::pow(g, j);
+        }
+    }
+}
+
+void updateTangCoefs(unsigned int level, unsigned int order) {
+    tang_coefs.resize(level);
+    tsrc_coefs.resize(level);
+    for (unsigned int i = 0; i < level; ++i) {
+        tang_coefs[i].resize(order);
+        tsrc_coefs[i].resize(order + 1);
+        float g = inv_tanNorm * i;
+        float f = 1.f - g;
+        for (unsigned int j = 0; j <= order - 1; ++j) {
+            tang_coefs[i][j] = pascal[order - 1][j] * std::pow(f, order - 1 - j) * std::pow(g, j);
+            tang_coefs[i][j] *= (order);
+        }
+
+        for (unsigned int j = 0; j <= order; ++j) {
+            tsrc_coefs[i][j] = pascal[order][j] * std::pow(f, order - j) * std::pow(g, j);
+        }
+    }
+}
+
 void updateVertexPoint(int idx) {
-	for (unsigned int i = 0; i <= smoothness; ++i) {
-		allPoints[idx * smoothness + i].position = 
-		make_curve(ctrlPoints[idx * 2].position,
-			ctrlPoints[idx * 2 + 1].position,
-			ctrlPoints[idx * 2 + 2].position,
-			inv_smoothness * i);
-	}
+    for (unsigned int i = 0; i <= smoothness; ++i) {
+        allPoints[idx * smoothness + i] = zero_vector;
+        for (unsigned int j = 0; j <= curve_order; ++j) {
+            // sums the polynomial
+            allPoints[idx * smoothness + i].position +=
+                ctrlPoints[idx * curve_order + j].position * poly_coefs[i][j];    
+        }
+    }
+}
+
+void updateTangentPoint(int idx) {
+    sf::Vector2f v, vn;
+    int tanPtLoc, ptLoc;
+    for (unsigned int i = 0; i < tanNorm; ++i) {
+        tanPtLoc = (idx * tanNorm + i) * 2;
+        ptLoc = idx * curve_order;
+
+        tanPoints[tanPtLoc] = zero_vector;
+        for (unsigned int j = 0; j <= curve_order; ++j) {
+            tanPoints[tanPtLoc].position += (ctrlPoints[ptLoc + j].position) * tsrc_coefs[i][j];
+        }
+        normalPoints[tanPtLoc].position = tanPoints[tanPtLoc].position;
+
+        // TODO: computation bug somewhere
+        tanPoints[tanPtLoc+1] = zero_vector;
+        for (unsigned int j = 0; j <= curve_order - 1; ++j) {
+            tanPoints[tanPtLoc+1].position += (ctrlPoints[ptLoc + j + 1].position) * tang_coefs[i][j];
+            tanPoints[tanPtLoc+1].position -= (ctrlPoints[ptLoc + j].position) * tang_coefs[i][j];
+        }
+
+        // normalizing the line
+        v = tanPoints[tanPtLoc+1].position;
+        v /= std::hypot(v.x, v.y);
+        v *= 20.f;
+        vn.x = -v.y;
+        vn.y = v.x;
+        tanPoints[tanPtLoc+1].position = tanPoints[tanPtLoc].position + v;
+        normalPoints[tanPtLoc+1].position = normalPoints[tanPtLoc].position + vn;
+
+        // setting the color of the lines
+        tanPoints[tanPtLoc].color = sf::Color::Red;
+        tanPoints[tanPtLoc+1].color = sf::Color::Red;
+        normalPoints[tanPtLoc].color = sf::Color::Blue;
+        normalPoints[tanPtLoc+1].color = sf::Color::Blue;
+    }
 }
 
 bool readFromAvailableText() {
     std::string input;
-    std::ifstream settings("hw03.txt");
+    std::ifstream settings("hw05.txt");
     if (settings.is_open()) {
+        settings >> curve_order;
     	settings >> smoothness;
+        settings >> tanNorm;
     	settings >> control_points;
     	circles.resize(control_points);
     	float x, y;
@@ -119,9 +206,9 @@ bool readFromAvailableText() {
 
 void initializeSettings() {
     if (readFromAvailableText()) {
-        std::cout << "hw03.txt successfully loaded.\n";
+        std::cout << "hw05.txt successfully loaded.\n";
     } else {
-        std::cout << "hw03.txt not loaded. Using default values.\n";
+        std::cout << "hw05.txt not loaded. Using default values.\n";
         circles.resize(control_points);
     	for (unsigned int i = 0; i < control_points; ++i) {
     		circles[i].setRadius(c_radius);
@@ -130,16 +217,19 @@ void initializeSettings() {
     	}
     }
 
-    // __NOTES__
-    // preparing variables
-    // won't explain why these unless needed
-    // trace/try the equations first, you'll be able to figure it out
     inv_smoothness = 1.f/smoothness;
-    curves = (control_points-1)/2;
+    curves = (control_points-1)/curve_order;
     points = curves * smoothness + 1;
+    tan_points = (curves * tanNorm) * 2;
+    inv_tanNorm = 1.f / (tanNorm - 1);
     ctrlPoints.resize(control_points);
     allPoints.resize(points);
+    tanPoints.resize(tan_points);
+    normalPoints.resize(tan_points);
     circlesFlags.resize(control_points);
+
+    updatePolyCoefs(smoothness, curve_order);
+    updateTangCoefs(tanNorm, curve_order);
 
     for (unsigned int i = 0; i < control_points; ++i) {
     	ctrlPoints[i].position = circles[i].getPosition();
@@ -151,6 +241,7 @@ void initializeSettings() {
 
     for (unsigned int i = 0; i < curves; ++i) {
     	updateVertexPoint(i);
+        updateTangentPoint(i);
     }
 }
 
@@ -223,14 +314,12 @@ void handleInput(sf::RenderWindow& window) {
     }
 }
 
-
 void update(const sf::Time& elapsed, sf::RenderWindow& window) {
     float delta = elapsed.asSeconds();
 
     sf::Vector2f mousePosition = sf::Vector2f(sf::Mouse::getPosition(window));
     sf::Vector2f placeholder;
 
-    // __NOTES__
     if (leftMouseButtonFlag) {
     	for (int i = 0; i < control_points; ++i) {
     		placeholder = circles[i].getPosition() - mousePosition;
@@ -239,15 +328,13 @@ void update(const sf::Time& elapsed, sf::RenderWindow& window) {
     			ctrlPoints[i].position = mousePosition;
     			circles[i].setPosition(mousePosition);
     			circlesFlags[i] = true;
-                // we use i/2 because we compute starting from the start of the curve
-                // technically speaking, it's really i/order_of_curve, but for this
-                // exercise, the order of the curve is always 2
+                // TODO: updateTangentPoint lazily
     			if (i%2 == 0) {
-                    // update the vertex point located to the left of i
-    				updateVertexPoint(std::max(i/2 - 1, 0));
+    				updateVertexPoint(std::max(i/int(curve_order) - 1, 0));
+                    updateTangentPoint(std::max(i/int(curve_order) - 1, 0));
     			}
-                // update the vertex point located to the right of i
-    			updateVertexPoint(std::min(i/2, curves-1));
+    			updateVertexPoint(std::min(i/int(curve_order), int(curves)-1));
+                updateTangentPoint(std::min(i/int(curve_order), int(curves)-1));
     			break;
     		}
     	}
@@ -260,12 +347,14 @@ void render(sf::RenderWindow& window) {
     	window.draw(circles[i]);
     }
     window.draw(allPoints);
+    window.draw(tanPoints);
+    window.draw(normalPoints);
     window.display();
 }
 
 int main () {
     srand(time(NULL));
-    sf::RenderWindow window(sf::VideoMode(window_w, window_h), "HW03");
+    sf::RenderWindow window(sf::VideoMode(window_w, window_h), "HW05");
 	window.setFramerateLimit(fps_limit);
 
     initializeSettings();
